4ef056405f867d2263eee713d341b9e0dd931ceb
Eason Cai <Eason.Cai@CDP.COM>
2018-06-01 16:13:27 +0800

    updata test model

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from config import config
 bootstrap=Bootstrap()
 db=SQLAlchemy()
 
-def creat_app(config_name):
+def create_app(config_name):
 	app=Flask(__name__)
 	app.config.from_object(config[config_name])
 	config[config_name].init_app(app)
