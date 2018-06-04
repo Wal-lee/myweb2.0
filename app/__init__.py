8073ee4a83b6a3d4ffb68c435622d1136a3a08c6
Eason Cai <798814817@qq.com>
2018-06-04 22:27:04 +0800

    clean error

--- app/__init__.py
+++ app/__init__.py
@@ -17,12 +17,13 @@ def create_app(config_name):
 
 	bootstrap.init_app(app)
 	db.init_app(app)
-	login_manager.init(app)
+	login_manager.init_app(app)
 
 	from .main import main as main_blueprint
 	app.register_blueprint(main_blueprint)
+	
 	from .auth import auth as auth_blueprint
-	app.register_blueprint(auth_blueprint,url_prefix='/path')
+	app.register_blueprint(auth_blueprint,url_prefix='/auth')
 
 	
 	return app
